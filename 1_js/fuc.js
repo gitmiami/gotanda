0005392ac2d15c8e5aeedf82f4b0cebcd9f89761
asatohc <36531226+asatohc@users.noreply.github.com>
2018-07-20 12:19:16 +0900

    1
    1

---
+++ 1_js/fuc.js
@@ -0,0 +1,25 @@
+
+<!--
+
+	var Cup =function(w){
+		this.amount = w;
+	};
+
+
+	var mycup = new Cup(300);
+
+	mycup.add=function(v){
+		this.amount +=v;
+	};
+
+
+mycup.add(50);
+
+document.write(mycup.amount + "ml “ü‚Á‚Ä‚¢‚Ü‚·")G
+
+//-->
+	
+
+
+
+
